d1f139eddcc5ed2e0bba58154e559b1b8876fe71
Antonio Saiful Islam <finallyantonio@gmail.com>
2023-03-27 22:11:17 +0700

    init

---
+++ src/test/java/com/edustar/config/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edustar.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
